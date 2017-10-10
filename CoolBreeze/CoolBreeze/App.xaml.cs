4747ab2bb6375d53bdbf94f90815f2cc940594bf
ISD SDDPT5 (HPoblete) <poblhat@insular.com.ph>
2017-10-10 12:39:04 +0800

    Add project files.

---
+++ CoolBreeze/CoolBreeze/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace CoolBreeze
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel;
+        public static string RegistrationCode = "XBWWA1A0D6CC1D5C77B239BC0B274844";
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new StartPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            MobileCenter.Start(typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
